53c4eec05e620902d93ef7d03394ff4977be6244
Blake Simpson <iblakesimpson@gmail.com>
2026-02-25 17:02:13 +1300

    update delete

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,42 +1,54 @@
 import { useState, useEffect } from "react";
-import "./App.css";
 import ContactList from "./ContactList";
+import "./App.css";
 import ContactForm from "./ContactForm";
 
 function App() {
   const [contacts, setContacts] = useState([]);
-  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [isModalOpen, setIsModalOpen] = useState(false)
+  const [currentContact, setCurrentContact] = useState({})
 
   useEffect(() => {
-    fetchContacts();
+    fetchContacts()
   }, []);
 
   const fetchContacts = async () => {
-    const response = await fetch("http://127.0.0.1:5000//contacts");
+    const response = await fetch("http://127.0.0.1:5000/contacts");
     const data = await response.json();
     setContacts(data.contacts);
   };
 
   const closeModal = () => {
-    setIsModalOpen(false);
-  };
+    setIsModalOpen(false)
+    setCurrentContact({})
+  }
 
   const openCreateModal = () => {
-    if (!isModalOpen) setIsModalOpen(true);
-  };
+    if (!isModalOpen) setIsModalOpen(true)
+  }
+
+  const openEditModal = (contact) => {
+    if (isModalOpen) return
+    setCurrentContact(contact)
+    setIsModalOpen(true)
+  }
+
+  const onUpdate = () => {
+    closeModal()
+    fetchContacts()
+  }
 
   return (
     <>
-      <ContactList contacts={contacts} />
+      <ContactList contacts={contacts} updateContact={openEditModal} updateCallback={onUpdate} />
       <button onClick={openCreateModal}>Create New Contact</button>
-      {isModalOpen && (
-        <div class="modal">
-          <div class="modal-content">
-            <span class="close" onClick={closeModal}>&times;</span>
-            <ContactForm />
-          </div>
+      {isModalOpen && <div className="modal">
+        <div className="modal-content">
+          <span className="close" onClick={closeModal}>&times;</span>
+          <ContactForm existingContact={currentContact} updateCallback={onUpdate} />
         </div>
-      )}
+      </div>
+      }
     </>
   );
 }
